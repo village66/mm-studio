fed94ac76427df719c1de20d0e0f753b6102fd88
Ken <mm.interdesign@gmail.com>
2026-07-23 14:46:54 +0800

    Update website

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -3,21 +3,24 @@ import { ReactNode } from "react";
 type Props = {
   children: ReactNode;
   className?: string;
-  cleanPadding?: boolean; // 新增選項：若特定版面想要完全無邊框貼邊可設為 true
 };
 
 export default function Container({
   children,
   className = "",
-  cleanPadding = false,
 }: Props) {
   return (
     <div
       className={`
         mx-auto
         w-full
-        max-w-[100vw]
-        ${cleanPadding ? "px-0" : "px-4 sm:px-8 md:px-12 lg:px-16 2xl:px-20"}
+        max-w-[1800px]
+        px-6
+        sm:px-8
+        md:px-10
+        lg:px-14
+        xl:px-20
+        2xl:px-24
         ${className}
       `}
     >
